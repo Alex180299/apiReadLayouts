af7d496e30a9d7004110604fb079f1aaa074cbec
Alejandro Ortiz <alejandroortiz@Mac-mini-de-Alejandro.local>
2020-06-09 11:54:43 -0500

    Agregando validaciones

--- src/main/java/com/read/api/controller/FiltersController.java
+++ src/main/java/com/read/api/controller/FiltersController.java
@@ -4,7 +4,9 @@ import com.read.app.model.Filter;
 import com.read.api.service.FiltersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
 import java.util.List;
 
 @RestController
@@ -21,25 +23,25 @@ public class FiltersController
     }
 
     @PostMapping("/filter")
-    public String saveFilter(@RequestBody Filter filter)
+    public String saveFilter(@RequestBody @Valid Filter filter)
     {
         return "{ " + filtersService.save(filter) + " }";
     }
 
     @GetMapping("/filter/{id}")
-    public Filter getFilterById(@PathVariable Long id)
+    public Filter getFilterById(@PathVariable @NotNull @Positive Long id)
     {
         return filtersService.findById(id);
     }
 
     @DeleteMapping("/filter/{id}")
-    public String deleteFilter(@PathVariable Long id)
+    public String deleteFilter(@PathVariable @NotNull @Positive Long id)
     {
         return "{ + " + filtersService.delete(id) + " }";
     }
 
     @PutMapping("/filter")
-    public String updateFilter(@RequestBody Filter filter)
+    public String updateFilter(@RequestBody @Valid Filter filter)
     {
         return "{ + " + filtersService.update(filter) + " }";
     }
--- src/main/java/com/read/api/controller/LayoutController.java
+++ src/main/java/com/read/api/controller/LayoutController.java
@@ -5,6 +5,9 @@ import com.read.app.model.Field;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
 import java.util.List;
 
 @RestController("/layout")
@@ -27,49 +30,49 @@ public class LayoutController
     }
 
     @GetMapping("/in/field/{id}")
-    public Field getInFieldById(@PathVariable Long id)
+    public Field getInFieldById(@PathVariable @NotNull @Positive Long id)
     {
         return layoutServiceImp.findInFieldById(id);
     }
 
     @GetMapping("/out/field/{id}")
-    public Field getOutFieldById(@PathVariable Long id)
+    public Field getOutFieldById(@PathVariable @NotNull @Positive Long id)
     {
         return layoutServiceImp.findOutFieldById(id);
     }
 
     @PostMapping("/in/field")
-    public String saveInField(@RequestBody Field field)
+    public String saveInField(@RequestBody @Valid Field field)
     {
         return "{ " + layoutServiceImp.saveInField(field) + " }";
     }
 
     @PostMapping("/out/field")
-    public String saveOutField(@RequestBody Field field)
+    public String saveOutField(@RequestBody @Valid Field field)
     {
         return "{ " + layoutServiceImp.saveOutField(field) + " }";
     }
 
     @DeleteMapping("/in/field/{id}")
-    public String deleteInField(@PathVariable Long id)
+    public String deleteInField(@PathVariable @NotNull @Positive Long id)
     {
         return "{ " + layoutServiceImp.deleteInField(id) + " }";
     }
 
     @DeleteMapping("/out/field/{id}")
-    public String deleteOutField(@PathVariable Long id)
+    public String deleteOutField(@PathVariable @NotNull @Positive Long id)
     {
         return "{ " + layoutServiceImp.deleteOutField(id) + " }";
     }
 
     @PutMapping("/in/field")
-    public String updateInField(@RequestBody Field field)
+    public String updateInField(@RequestBody @Valid Field field)
     {
         return "{ " + layoutServiceImp.updateInField(field) + " }";
     }
 
     @PutMapping("/out/field")
-    public String updateOutField(@RequestBody Field field)
+    public String updateOutField(@RequestBody @Valid Field field)
     {
         return "{ " + layoutServiceImp.updateOutField(field) + " }";
     }
